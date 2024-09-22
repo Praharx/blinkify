833c4d596ea260f2b2712608536bc47e036eb728
harshana15 <harshana555prajapati@gmail.com>
2024-09-22 23:41:08 +0530

    made nextjs config changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
-  setupDevPlatform();
+  await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
@@ -22,4 +22,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
