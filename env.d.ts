47a2c020031338f07378ed3435ee0b11b376fb54
harshana15 <harshana555prajapati@gmail.com>
2024-09-23 12:34:32 +0530

    added content

--- env.d.ts
+++ env.d.ts
@@ -3,3 +3,16 @@
 
 interface CloudflareEnv {
 }
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+        CLERK_SECRET_KEY: string;
+        NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL: string;
+        NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: string;
+        NEXT_PUBLIC_CLOUDFRONT_URL: string;
+        NEXT_PUBLIC_ACCESS_KEY_ID: string;
+        NEXT_PUBLIC_ACCESS_KEY_PASSWORD: string;
+        NEXT_PUBLIC_CLERK_JWT_KEY: string;
+    }
+  }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL,
+    NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL,
+    NEXT_PUBLIC_CLOUDFRONT_URL: process.env.NEXT_PUBLIC_CLOUDFRONT_URL,
+    NEXT_PUBLIC_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+    NEXT_PUBLIC_ACCESS_KEY_PASSWORD: process.env.NEXT_PUBLIC_ACCESS_KEY_PASSWORD,
+    NEXT_PUBLIC_CLERK_JWT_KEY: process.env.NEXT_PUBLIC_CLERK_JWT_KEY,
+  },
   images: {
     domains: [
       "images.unsplash.com", 
@@ -11,9 +21,14 @@ const nextConfig = {
   experimental: {
     workerThreads: false,
     cpus: 1
-  },
-  // Add this line
-  runtime: 'edge',
+  }
 };
+
+if (process.env.NODE_ENV === 'development') {
+  // Dynamic import for development setup
+  import('@cloudflare/next-on-pages/next-dev').then(({ setupDevPlatform }) => {
+    setupDevPlatform();
+  });
+}
 
 export default nextConfig;
